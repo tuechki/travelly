88ae819bd4b1a6c84f972089fe8dc0bb1d90e050
psm2001 <panayot.marinov12@gmail.com>
2023-05-24 13:22:03 +0300

    Initial project setup

---
+++ src/test/java/com/sofia/uni/fmi/travelly/TravellyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofia.uni.fmi.travelly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
