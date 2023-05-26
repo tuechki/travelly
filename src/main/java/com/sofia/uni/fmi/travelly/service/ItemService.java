6a64ba5175ad2eb1f3d354680fa33d100666e9f5
psm2001 <panayot.marinov12@gmail.com>
2023-05-26 20:21:50 +0300

    Created ItemService class with update and delete methods.

---
+++ src/main/java/com/sofia/uni/fmi/travelly/service/ItemService.java
@@ -0,0 +1,22 @@
+package com.sofia.uni.fmi.travelly.service;
+
+import com.sofia.uni.fmi.travelly.dto.ItemDto;
+import com.sofia.uni.fmi.travelly.model.Item;
+import com.sofia.uni.fmi.travelly.repository.ItemRepository;
+
+public class ItemService {
+    private ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public Item updateItemById(Long itemId, Item item) {
+        Item savedItem = itemRepository.save(item);
+        return savedItem;
+    }
+
+    public void deleteItemById(Long itemId) {
+        itemRepository.deleteById(itemId);
+    }
+}
