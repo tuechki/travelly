f2a1ecf43d0b5027a084d67502a298251d947bdc
psm2001 <panayot.marinov12@gmail.com>
2023-06-04 16:52:14 +0300

    Added ActivityDto class

---
+++ src/main/java/com/sofia/uni/fmi/travelly/dto/ActivityDto.java
@@ -0,0 +1,24 @@
+package com.sofia.uni.fmi.travelly.dto;
+
+import com.sofia.uni.fmi.travelly.model.ActivityType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityDto {
+    private Long id;
+    private Itinerary itinerary;
+    private ActivityType activityType;
+    private String activityLocation;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String description;
+}
