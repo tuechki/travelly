6f8feb7d454eb2d28e29361b15930aa0188ac013
psm2001 <panayot.marinov12@gmail.com>
2023-06-04 16:52:30 +0300

    Added ActivityService class

---
+++ src/main/java/com/sofia/uni/fmi/travelly/controller/ActivityController.java
@@ -0,0 +1,33 @@
+package com.sofia.uni.fmi.travelly.controller;
+
+import com.sofia.uni.fmi.travelly.ActivityMapper.ActivityMapper;
+import com.sofia.uni.fmi.travelly.dto.ActivityDto;
+import com.sofia.uni.fmi.travelly.model.Activity;
+import com.sofia.uni.fmi.travelly.service.ActivityService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("trips")
+public class ActivityController {
+    private ActivityService activityService;
+    private ActivityMapper activityMapper;
+
+    public ActivityController(ActivityService activityService, ActivityMapper activityMapper) {
+        this.activityService = activityService;
+        this.activityMapper = activityMapper;
+    }
+
+    @PutMapping("{activityId}")
+    public ActivityDto updateActivityById(@RequestParam Long activityId, @RequestBody ActivityDto activityDto) {
+        Activity activity = activityMapper.toEntity(activityDto);
+        Activity updatedActivity = activityService.updateActivityById(activityId, activity);
+        ActivityDto updatedActivityDto = activityMapper.toDto(updatedActivity);
+
+        return updatedActivityDto;
+    }
+
+    @DeleteMapping("{activityId}")
+    public void deleteActivityById(@RequestParam Long activityId) {
+        activityService.deleteActivityById(activityId);
+    }
+}
