3c13934c898da9234d6c11befc1c5395dd0f245a
psm2001 <panayot.marinov12@gmail.com>
2023-06-04 16:47:51 +0300

    Removed unused imports

--- src/main/java/com/sofia/uni/fmi/travelly/repository/ActivityRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/ActivityRepository.java
@@ -1,7 +1,6 @@
 package com.sofia.uni.fmi.travelly.repository;
 
 import com.sofia.uni.fmi.travelly.model.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
