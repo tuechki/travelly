9e570ad8d03ef30065b1e89d93700c29368cb19d
psm2001 <panayot.marinov12@gmail.com>
2023-06-13 10:45:23 +0300

    Created Itinerary controller class

---
+++ src/main/java/com/sofia/uni/fmi/travelly/controller/ItineraryController.java
@@ -0,0 +1,34 @@
+package com.sofia.uni.fmi.travelly.controller;
+
+import com.sofia.uni.fmi.travelly.dto.ItineraryDto;
+import com.sofia.uni.fmi.travelly.mapper.ItineraryMapper;
+import com.sofia.uni.fmi.travelly.model.Itinerary;
+import com.sofia.uni.fmi.travelly.service.ItineraryService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("itineraries")
+public class ItineraryController {
+    private ItineraryService itineraryService;
+    private ItineraryMapper itineraryMapper;
+
+    public ItineraryController(ItineraryService itineraryService, ItineraryMapper itineraryMapper) {
+        this.itineraryService = itineraryService;
+        this.itineraryMapper = itineraryMapper;
+    }
+
+    @PutMapping("{itineraryId}")
+    public ItineraryDto updateItinerary(@RequestParam Long itineraryId, @RequestBody ItineraryDto itineraryDto) {
+        Itinerary itinerary = itineraryMapper.toEntity(itineraryDto);
+        itinerary.setId(itineraryId);
+        Itinerary updatedItinerary = itineraryService.updateItinerary(itinerary);
+        ItineraryDto updatedItineraryDto = itineraryMapper.toDto(updatedItinerary);
+
+        return updatedItineraryDto;
+    }
+
+    @DeleteMapping("{itineraryId}")
+    public void deleteItinerary(@RequestParam Long itineraryId) {
+        itineraryService.deleteItinerary(itineraryId);
+    }
+}
