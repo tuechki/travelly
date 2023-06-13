0ca34e37cb717d56a99c9fadd37826e574db3b0d
psm2001 <panayot.marinov12@gmail.com>
2023-06-13 10:44:54 +0300

    Created Itinerary entity class

---
+++ src/main/java/com/sofia/uni/fmi/travelly/model/Itinerary.java
@@ -0,0 +1,24 @@
+package com.sofia.uni.fmi.travelly.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Itinerary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="trip_id")
+    private Trip trip;
+    @Column
+    private int dayNum;
+}
