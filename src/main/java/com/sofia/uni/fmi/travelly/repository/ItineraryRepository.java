cd68fb8e0bcfc77700b98a6d9e04769af028351b
psm2001 <panayot.marinov12@gmail.com>
2023-06-13 10:52:03 +0300

    Created Itinerary repository interface

---
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/ItineraryRepository.java
@@ -0,0 +1,9 @@
+package com.sofia.uni.fmi.travelly.repository;
+
+import com.sofia.uni.fmi.travelly.model.Itinerary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
+}
