c141498d354f8992c5328ca675b80634ebf4bd8e
psm2001 <panayot.marinov12@gmail.com>
2023-08-12 14:45:44 +0300

    Fixed wrongly extended JpaRepository interface

--- src/main/java/com/sofia/uni/fmi/travelly/repository/AccommodationRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/AccommodationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccommodationRepository extends JpaRepository<Long, Accommodation> {
+public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
 }
