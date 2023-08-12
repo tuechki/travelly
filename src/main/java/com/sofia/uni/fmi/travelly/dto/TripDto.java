303ee2dec35d7bb64dfc572546c9c064cd647570
psm2001 <panayot.marinov12@gmail.com>
2023-08-12 15:05:44 +0300

    Some required imports made

--- src/main/java/com/sofia/uni/fmi/travelly/dto/TripDto.java
+++ src/main/java/com/sofia/uni/fmi/travelly/dto/TripDto.java
@@ -1,11 +1,14 @@
 package com.sofia.uni.fmi.travelly.dto;
 
+import com.sofia.uni.fmi.travelly.model.Item;
+import com.sofia.uni.fmi.travelly.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
 @Data
