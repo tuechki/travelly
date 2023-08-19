42c870d5901e6e55c2b77e7402e7ea19deebfc2a
tuechki <dtuechki@gmail.com>
2023-08-19 17:50:07 +0300

    Move item methods to itemService.

--- src/main/java/com/sofia/uni/fmi/travelly/service/ItemService.java
+++ src/main/java/com/sofia/uni/fmi/travelly/service/ItemService.java
@@ -1,21 +1,46 @@
 package com.sofia.uni.fmi.travelly.service;
 
 import com.sofia.uni.fmi.travelly.dto.ItemDto;
+import com.sofia.uni.fmi.travelly.mapper.ItemMapper;
 import com.sofia.uni.fmi.travelly.model.Item;
 import com.sofia.uni.fmi.travelly.repository.ItemRepository;
+import com.sofia.uni.fmi.travelly.repository.TripRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemService {
-    private ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
 
-    public ItemService(ItemRepository itemRepository) {
+    private final ItemMapper itemMapper;
+    private final TripRepository tripRepository;
+
+    public ItemService(ItemRepository itemRepository, ItemMapper itemMapper, TripRepository tripRepository) {
         this.itemRepository = itemRepository;
+        this.itemMapper = itemMapper;
+        this.tripRepository = tripRepository;
+    }
+
+    public List<Item> getItemsByTripId(Long tripId) {
+        return itemRepository.findAllByTrip(tripRepository.findById(tripId).get());
+    }
+
+
+    public Long addItem(ItemDto itemDto, Long tripId) {
+        Item newItem = itemMapper.toEntity(itemDto);
+        newItem.setId(tripId);
+        Item savedItem = itemRepository.save(newItem);
+
+        return savedItem.getId();
+    }
+
+    public void deleteAllItems(Long tripId) {
+        itemRepository.deleteAllByTrip(tripRepository.findById(tripId).get());
     }
 
     public Item updateItem(Item item) {
-        Item savedItem = itemRepository.save(item);
-        return savedItem;
+        return itemRepository.save(item);
     }
 
     public void deleteItem(Long itemId) {
