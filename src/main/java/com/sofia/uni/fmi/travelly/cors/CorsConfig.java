36e3574dc323e4896681b723aa58c7e66f4c3750
psm2001 <panayot.marinov12@gmail.com>
2023-08-20 12:15:55 +0300

    Added CorsConfig class in order to allow requests from the frontend

---
+++ src/main/java/com/sofia/uni/fmi/travelly/cors/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.sofia.uni.fmi.travelly.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all paths
+                .allowedOrigins("http://localhost:4200") // Replace with your Angular app's URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
