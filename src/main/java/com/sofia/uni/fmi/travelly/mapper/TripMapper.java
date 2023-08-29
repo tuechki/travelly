e57134a15a6f76e6a7f2786b6fdcc5e5b0fbda44
psm2001 <panayot.marinov12@gmail.com>
2023-08-29 21:10:22 +0300

    add toListDto method

--- src/main/java/com/sofia/uni/fmi/travelly/mapper/TripMapper.java
+++ src/main/java/com/sofia/uni/fmi/travelly/mapper/TripMapper.java
@@ -1,6 +1,7 @@
 package com.sofia.uni.fmi.travelly.mapper;
 
 import com.sofia.uni.fmi.travelly.dto.TripDto;
+import com.sofia.uni.fmi.travelly.dto.TripListDto;
 import com.sofia.uni.fmi.travelly.model.Trip;
 import org.springframework.stereotype.Component;
 
@@ -9,29 +10,40 @@ public class TripMapper {
 
     public TripDto toDto(Trip entity) {
         return TripDto.builder()
-            .id(entity.getId())
-            .name(entity.getName())
-            .startDate(entity.getStartDate())
-            .endDate(entity.getEndDate())
-            .budget(entity.getBudget())
-            .interests(entity.getInterests())
-            .items(entity.getItems())
-            .users(entity.getUsers())
-            .build();
+                .id(entity.getId())
+                .destination(entity.getDestination())
+                .name(entity.getName())
+                .startDate(entity.getStartDate())
+                .endDate(entity.getEndDate())
+                .budget(entity.getBudget())
+                .interests(entity.getInterests())
+//            .items(entity.getItems())
+//            .users(entity.getUsers())
+                .build();
+    }
+
+    public TripListDto toListDto(Trip entity) {
+        return TripListDto.builder()
+                .name(entity.getName())
+                .destination(entity.getDestination())
+                .startDate(entity.getStartDate())
+                .endDate(entity.getEndDate())
+                .build();
     }
 
 
     public Trip toEntity(TripDto dto) {
         return Trip.builder()
-            .id(dto.getId())
-            .name(dto.getName())
-            .startDate(dto.getStartDate())
-            .endDate(dto.getEndDate())
-            .budget(dto.getBudget())
-            .interests(dto.getInterests())
-            .items(dto.getItems())
-            .users(dto.getUsers())
-            .build();
+                .id(dto.getId())
+                .destination(dto.getDestination())
+                .name(dto.getName())
+                .startDate(dto.getStartDate())
+                .endDate(dto.getEndDate())
+                .budget(dto.getBudget())
+                .interests(dto.getInterests())
+//            .items(dto.getItems())
+//            .users(dto.getUsers())
+                .build();
     }
 
 
