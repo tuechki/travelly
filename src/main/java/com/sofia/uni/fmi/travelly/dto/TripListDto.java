884315d096f996ece94c5446fb0258d2b3e2fe35
psm2001 <panayot.marinov12@gmail.com>
2023-08-29 21:10:30 +0300

    add tripListDto

---
+++ src/main/java/com/sofia/uni/fmi/travelly/dto/TripListDto.java
@@ -0,0 +1,24 @@
+package com.sofia.uni.fmi.travelly.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TripListDto {
+
+    private String name;
+
+    private String destination;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+}
