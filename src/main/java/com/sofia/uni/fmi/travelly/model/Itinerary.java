de6c1e765bd52670beefa73ae08023217c6e3908
tuechki <dtuechki@gmail.com>
2023-08-29 12:26:58 +0300

    Add Table and Column annotations to Itinerary. Fix table relations.

--- src/main/java/com/sofia/uni/fmi/travelly/model/Itinerary.java
+++ src/main/java/com/sofia/uni/fmi/travelly/model/Itinerary.java
@@ -6,21 +6,33 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name="itineraries")
 public class Itinerary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    @Column(name = "id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name="trip_id")
     private Trip trip;
 
-    @Column
+    @OneToMany(mappedBy = "itinerary", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Accommodation> accommodations;
+
+    @OneToMany(mappedBy = "itinerary", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Activity> activities;
+
+    @OneToMany(mappedBy = "itinerary", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TransportationOption> transportationOptions;
+
+    @Column(name = "days")
     private int dayNum;
 }
