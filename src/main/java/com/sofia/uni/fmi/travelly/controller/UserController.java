732dd15bf351b6f5029b23da5b24e0e6c8a23391
psm2001 <panayot.marinov12@gmail.com>
2023-08-29 21:10:08 +0300

    add tripMapper and update getTrips return type

--- src/main/java/com/sofia/uni/fmi/travelly/controller/UserController.java
+++ src/main/java/com/sofia/uni/fmi/travelly/controller/UserController.java
@@ -1,24 +1,28 @@
 package com.sofia.uni.fmi.travelly.controller;
 
 import com.sofia.uni.fmi.travelly.dto.TripDto;
+import com.sofia.uni.fmi.travelly.dto.TripListDto;
 import com.sofia.uni.fmi.travelly.dto.UserDto;
+import com.sofia.uni.fmi.travelly.mapper.TripMapper;
 import com.sofia.uni.fmi.travelly.mapper.UserMapper;
 import com.sofia.uni.fmi.travelly.model.Trip;
 import com.sofia.uni.fmi.travelly.model.User;
 import com.sofia.uni.fmi.travelly.service.TripService;
 import com.sofia.uni.fmi.travelly.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
     private UserService service;
-    private TripService tripService;
-
+    private TripMapper tripMapper;
     private UserMapper userMapper;
 
     @GetMapping("/{id}")
@@ -53,13 +57,17 @@ public class UserController {
     }
 
     @GetMapping("/{id}/trips")
-    public List<Trip> getTrips(@PathVariable Long id) {
-        return service.getTrips(id);
+    public List<TripListDto> getTrips(@PathVariable Long id) {
+
+        return service.getTrips(id)
+                .stream()
+                .map(trip -> tripMapper.toListDto(trip))
+                .collect(Collectors.toList());
     }
 
-    @PostMapping("{id}/trips")
-    public Long addTrip(@PathVariable Long id, @RequestBody TripDto tripDto) {
-        User user = service.getUser(id);
-        return service.addTrip(id, tripDto);
-    }
+//    @PostMapping("{id}/trips")
+//    public Long addTrip(@PathVariable Long id, @RequestBody TripDto tripDto) {
+//        User user = service.getUser(id);
+//        return service.addTrip(id, tripDto);
+//    }
 }
