d8ca29c0edfd7c4b8d40c9fcdeb50facface6ab4
tuechki <dtuechki@gmail.com>
2023-08-31 21:14:11 +0300

    Add findByUsername method.

--- src/main/java/com/sofia/uni/fmi/travelly/repository/UserRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.sofia.uni.fmi.travelly.repository;
 
+import com.sofia.uni.fmi.travelly.model.Item;
+import com.sofia.uni.fmi.travelly.model.Trip;
 import com.sofia.uni.fmi.travelly.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
