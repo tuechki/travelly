efa659e5f1e3e27039cd33c1210594fb82e99820
tuechki <dtuechki@gmail.com>
2023-08-31 21:14:35 +0300

    Remove unused imports.

--- src/main/java/com/sofia/uni/fmi/travelly/repository/UserRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/UserRepository.java
@@ -1,12 +1,8 @@
 package com.sofia.uni.fmi.travelly.repository;
 
-import com.sofia.uni.fmi.travelly.model.Item;
-import com.sofia.uni.fmi.travelly.model.Trip;
 import com.sofia.uni.fmi.travelly.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
