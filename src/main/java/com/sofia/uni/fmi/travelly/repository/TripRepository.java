34d5e80b0b7bb9768987ab3a0e2e4b1a116664d0
tuechki <dtuechki@gmail.com>
2023-08-31 21:13:34 +0300

    Remove unused imports.

--- src/main/java/com/sofia/uni/fmi/travelly/repository/TripRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/TripRepository.java
@@ -1,14 +1,9 @@
 package com.sofia.uni.fmi.travelly.repository;
 
-import com.sofia.uni.fmi.travelly.model.Item;
 import com.sofia.uni.fmi.travelly.model.Trip;
-import lombok.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
-
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {
-
 }
