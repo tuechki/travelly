69f3221fad3983dce7f654d081404b9b9840249b
psm2001 <panayot.marinov12@gmail.com>
2023-09-01 19:24:16 +0300

    create ItemCreateDto

---
+++ src/main/java/com/sofia/uni/fmi/travelly/dto/ItemCreateDto.java
@@ -0,0 +1,20 @@
+package com.sofia.uni.fmi.travelly.dto;
+
+import com.sofia.uni.fmi.travelly.model.Trip;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemCreateDto {
+    private Long id;
+    private Trip trip;
+    private String name;
+    private String description;
+    private Double amount;
+    private boolean isPacked;
+}
