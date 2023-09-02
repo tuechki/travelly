48b5e6f56d40932ba7fb321d14e4f951e2b83932
psm2001 <panayot.marinov12@gmail.com>
2023-09-02 13:49:23 +0300

    create methods to find all activities of an itinerary and delete all activities of an itinerary

--- src/main/java/com/sofia/uni/fmi/travelly/repository/ActivityRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/ActivityRepository.java
@@ -1,9 +1,14 @@
 package com.sofia.uni.fmi.travelly.repository;
 
 import com.sofia.uni.fmi.travelly.model.Activity;
+import com.sofia.uni.fmi.travelly.model.Itinerary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    List<Activity> findAllByItinerary(Itinerary itinerary);
+    void deleteByItinerary(Itinerary itinerary);
 }
