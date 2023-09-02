12dac3502b982ba500e718b1fc44d387ac796c67
psm2001 <panayot.marinov12@gmail.com>
2023-09-02 14:19:13 +0300

    add methods to find all accommodations of an itinerary and delete all accommodations of an itinerary

--- src/main/java/com/sofia/uni/fmi/travelly/repository/AccommodationRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/AccommodationRepository.java
@@ -1,9 +1,14 @@
 package com.sofia.uni.fmi.travelly.repository;
 
 import com.sofia.uni.fmi.travelly.model.Accommodation;
+import com.sofia.uni.fmi.travelly.model.Itinerary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
+    List<Accommodation> findAllByItinerary(Itinerary itinerary);
+    void deleteByItinerary(Itinerary itinerary);
 }
