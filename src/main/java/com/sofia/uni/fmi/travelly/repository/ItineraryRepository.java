eea3f1fab5893593f9bf5b226bc2781df458e324
psm2001 <panayot.marinov12@gmail.com>
2023-09-02 13:03:05 +0300

    add methods to find Itineraries by trip and delete itineraries by trip

--- src/main/java/com/sofia/uni/fmi/travelly/repository/ItineraryRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/ItineraryRepository.java
@@ -1,9 +1,14 @@
 package com.sofia.uni.fmi.travelly.repository;
 
 import com.sofia.uni.fmi.travelly.model.Itinerary;
+import com.sofia.uni.fmi.travelly.model.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
+    List<Itinerary> findAllByTrip(Trip trip);
+    void deleteByTrip(Trip trip);
 }
