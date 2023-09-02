151afcd4ddb0406269f1cc256acb4a2628a63690
psm2001 <panayot.marinov12@gmail.com>
2023-09-02 13:51:20 +0300

    create getActivitiesByItineraryId, addActivity, deleteAllActivities methods with proper dependencies and imports

--- src/main/java/com/sofia/uni/fmi/travelly/controller/ItineraryController.java
+++ src/main/java/com/sofia/uni/fmi/travelly/controller/ItineraryController.java
@@ -1,11 +1,16 @@
 package com.sofia.uni.fmi.travelly.controller;
 
+import com.sofia.uni.fmi.travelly.dto.ActivityCreateUpdateDto;
 import com.sofia.uni.fmi.travelly.dto.ItineraryCreateUpdateDto;
 import com.sofia.uni.fmi.travelly.dto.ItineraryDto;
+import com.sofia.uni.fmi.travelly.mapper.ActivityMapper;
 import com.sofia.uni.fmi.travelly.mapper.ItineraryMapper;
 import com.sofia.uni.fmi.travelly.model.Itinerary;
+import com.sofia.uni.fmi.travelly.service.ActivityService;
 import com.sofia.uni.fmi.travelly.service.ItineraryService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("itineraries")
@@ -13,9 +18,20 @@ public class ItineraryController {
     private ItineraryService itineraryService;
     private ItineraryMapper itineraryMapper;
 
-    public ItineraryController(ItineraryService itineraryService, ItineraryMapper itineraryMapper) {
+    private ActivityService activityService;
+    private ActivityMapper activityMapper;
+
+
+    public ItineraryController(
+            ItineraryService itineraryService,
+            ItineraryMapper itineraryMapper,
+            ActivityService activityService,
+            ActivityMapper activityMapper
+    ) {
         this.itineraryService = itineraryService;
         this.itineraryMapper = itineraryMapper;
+        this.activityService = activityService;
+        this.activityMapper = activityMapper;
     }
 
     @PatchMapping("{itineraryId}")
@@ -32,5 +48,25 @@ public class ItineraryController {
     @DeleteMapping("{itineraryId}")
     public void deleteItinerary(@PathVariable Long itineraryId) {
         itineraryService.deleteItinerary(itineraryId);
+    }
+
+    @GetMapping("{itineraryId}/activities")
+    public List<ActivityCreateUpdateDto> getActivitiesByItineraryId(@PathVariable Long itineraryId) {
+        return activityService.getActivitiesByItineraryId(itineraryId)
+                .stream()
+                .map(activity -> activityMapper.toCreateUpdateDto(activity))
+                .toList();
+    }
+
+    @PostMapping("{itineraryId}/activities")
+    public void addActivity(
+            @PathVariable Long itineraryId,
+            @RequestBody ActivityCreateUpdateDto activityCreateUpdateDto) {
+        activityService.addActivity(activityCreateUpdateDto, itineraryId);
+    }
+
+    @DeleteMapping("{itineraryId}/activities")
+    public void deleteAllActivities(@PathVariable Long itineraryId) {
+        activityService.deleteAllActivities(itineraryId);
     }
 }
