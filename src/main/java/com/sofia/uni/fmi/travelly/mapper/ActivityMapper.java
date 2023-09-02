e3f3da440e7044fd2ebfc70d149effda82ec340a
psm2001 <panayot.marinov12@gmail.com>
2023-09-02 13:48:49 +0300

    create methods to map Activity to ActivityCreateUpdateDto and vice versa

--- src/main/java/com/sofia/uni/fmi/travelly/mapper/ActivityMapper.java
+++ src/main/java/com/sofia/uni/fmi/travelly/mapper/ActivityMapper.java
@@ -1,5 +1,6 @@
 package com.sofia.uni.fmi.travelly.mapper;
 
+import com.sofia.uni.fmi.travelly.dto.ActivityCreateUpdateDto;
 import com.sofia.uni.fmi.travelly.dto.ActivityDto;
 import com.sofia.uni.fmi.travelly.model.Activity;
 import org.springframework.stereotype.Component;
@@ -27,6 +28,26 @@ public class ActivityMapper {
                 .startTime(activityDto.getStartTime())
                 .endTime(activityDto.getEndTime())
                 .description(activityDto.getDescription())
+                .build();
+    }
+
+    public ActivityCreateUpdateDto toCreateUpdateDto(Activity activity) {
+        return ActivityCreateUpdateDto.builder()
+                .activityType(activity.getActivityType())
+                .activityLocation(activity.getActivityLocation())
+                .startTime(activity.getStartTime())
+                .endTime(activity.getEndTime())
+                .description(activity.getDescription())
+                .build();
+    }
+
+    public Activity toEntity(ActivityCreateUpdateDto activityCreateUpdateDto) {
+        return Activity.builder()
+                .activityType(activityCreateUpdateDto.getActivityType())
+                .activityLocation(activityCreateUpdateDto.getActivityLocation())
+                .startTime(activityCreateUpdateDto.getStartTime())
+                .endTime(activityCreateUpdateDto.getEndTime())
+                .description(activityCreateUpdateDto.getDescription())
                 .build();
     }
 }
