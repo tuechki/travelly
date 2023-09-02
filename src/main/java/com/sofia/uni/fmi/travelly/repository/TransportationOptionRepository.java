2ab9a072de139f9ee9f63f794cdc4b3eab560d4e
psm2001 <panayot.marinov12@gmail.com>
2023-09-02 20:20:37 +0300

    add a method to find transportation options by certain criteria

--- src/main/java/com/sofia/uni/fmi/travelly/repository/TransportationOptionRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/TransportationOptionRepository.java
@@ -1,8 +1,12 @@
 package com.sofia.uni.fmi.travelly.repository;
 
+import com.sofia.uni.fmi.travelly.model.Accommodation;
+import com.sofia.uni.fmi.travelly.model.Activity;
 import com.sofia.uni.fmi.travelly.model.Itinerary;
 import com.sofia.uni.fmi.travelly.model.TransportationOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +16,7 @@ public interface TransportationOptionRepository extends JpaRepository<Transporta
     List<TransportationOption> findAllByItinerary(Itinerary itinerary);
 
     void deleteByItinerary(Itinerary itinerary);
+
+    @Query("SELECT a FROM TransportationOption a WHERE a.price <= :budget")
+    List<TransportationOption> findTransportationOptionsByCriteria(@Param("budget") Double budget);
 }
