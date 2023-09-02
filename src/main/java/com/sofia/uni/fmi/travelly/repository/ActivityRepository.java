990dcd65617a8ece31f6aecfa6dc85733b777416
psm2001 <panayot.marinov12@gmail.com>
2023-09-02 19:37:52 +0300

    add method which finds activities by criteria from repository

--- src/main/java/com/sofia/uni/fmi/travelly/repository/ActivityRepository.java
+++ src/main/java/com/sofia/uni/fmi/travelly/repository/ActivityRepository.java
@@ -1,14 +1,28 @@
 package com.sofia.uni.fmi.travelly.repository;
 
+import com.sofia.uni.fmi.travelly.dto.ActivityDto;
 import com.sofia.uni.fmi.travelly.model.Activity;
 import com.sofia.uni.fmi.travelly.model.Itinerary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
     List<Activity> findAllByItinerary(Itinerary itinerary);
     void deleteByItinerary(Itinerary itinerary);
+
+    @Query("SELECT a FROM Activity a WHERE " +
+            "(a.activityLocation = :location OR a.description LIKE CONCAT('%', :interest, '%')) " +
+            "AND a.startTime > :startTime " +
+            "AND a.endTime < :endTime ")
+    List<Activity> findActivitiesByCriteria(
+            @Param("location") String location,
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime,
+            @Param("interest") String interests);
 }
